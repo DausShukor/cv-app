c1cc24b00ab939f6ae7c121653c472512024d147
Muhammad Firdaus Bin Abdul Shukor <firdaus.shukor@student.gamudaai.academy>
2026-05-08 11:41:40 +0800

    fix: remove user authentication checks from Media access configuration

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -4,9 +4,6 @@ export const Media: CollectionConfig = {
   slug: 'media',
   access: {
     read: () => true,
-    create: ({ req }) => Boolean(req.user),
-    update: ({ req }) => Boolean(req.user),
-    delete: ({ req }) => Boolean(req.user),
   },
   fields: [
     {
