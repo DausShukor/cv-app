86661cd03b61a621ff2adeea8c80bff04178b48e
Muhammad Firdaus Bin Abdul Shukor <firdaus.shukor@student.gamudaai.academy>
2026-05-08 09:34:23 +0800

    fix: add migration to replace profile photo_id with photo_url

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260508_013319 from './20260508_013319';
+
+export const migrations = [
+  {
+    up: migration_20260508_013319.up,
+    down: migration_20260508_013319.down,
+    name: '20260508_013319'
+  },
+];
